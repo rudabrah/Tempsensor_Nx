d3bfebbe6ff2b91bc60630c9351e48621b4dc20e
Rudi Abrahamsen <47385921+rudabrah@users.noreply.github.com>
2022-09-09 13:05:29 +0200

    Playing with the solution. Added avg, min & max

--- temps.py
+++ temps.py
@@ -3,22 +3,40 @@ import numpy
 import json
 import time
 
-i = 0
-payload = []
-r = requests.get('https://httpbin.org/get')
 
-with open('temperature.txt', 'r') as malinger:
-    
-    
-    for line in malinger:
-        payload.append((100/4095)*float(line)-50)
-        try: 
-            payload.pop(1199)
-        except:
-             print('under 1200 målinger')
-        print(payload[i])
-        if r.status_code == 200:
-            post = requests.post('https://httpbin.org/post', data=str(payload[i]))
-            print(post.text)
+
+payload = []
+backup=[]
+listSpaces = 120/0.1 - 1
+
+
+r = requests.get('https://httpbin.org/get')#just to make sure there is contact
+
+
+
+
+def getTemperature():
+    i = 0
+    with open('temperature.txt', 'r') as malinger:
+        for line in malinger:
             time.sleep(0.1)
-        i+=1
+            payload.append((100/4095)*float(line)-50)#quick and dirty way to get temperatures.
+            if i >= listSpaces:#make sure the list remains at 2 mins. Min, Max and avg only need an update at 2min intervals
+                avgTemp = numpy.mean(payload)
+                payload.clear()
+                print("Cleared")
+                i = 0
+            else:
+                maxlist = max(payload)
+                minlist = min(payload)
+                print(numpy.mean(payload), maxlist, minlist)
+                i+=1
+
+getTemperature()
+
+#if r.status_code == 200:
+#    post = requests.post('https://httpbin.org/post', data=str(payload[i]))
+#    print(post.text)
+#    time.sleep(0.1)
+#elif r.status_code == 500:
+#    post = requests.post('https://httpbin.org/dump', data=str(payload[i]))
