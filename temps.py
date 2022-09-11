854c323abdc4ffbe6c764e59b01a46e354f31e88
Rudi Abrahamsen <47385921+rudabrah@users.noreply.github.com>
2022-09-11 21:25:38 +0200

    Redid most everything. Should be a working product

--- temps.py
+++ temps.py
@@ -1,42 +1,101 @@
 import requests
-import numpy
-import json
+import numpy as np
 import time
+import datetime
 
 
-
-payload = []
-backup=[]
-listSpaces = 120/0.1 - 1
-
-
-r = requests.get('https://httpbin.org/get')#just to make sure there is contact
-
-
+targetUrl = 'https://httpbin.org/get'
+backupUrl = 'https://httpbin.org/dump'
+infile = 'temperature.txt'
+totbits = 4096  # 12bit ADC
+slpTime_reads = 0.10
+intervalTime = 1  # timeinterval in sec
+prev = 0
 
 
-def getTemperature():
-    i = 0
-    with open('temperature.txt', 'r') as malinger:
-        for line in malinger:
-            time.sleep(0.1)
-            payload.append((100/4095)*float(line)-50)#quick and dirty way to get temperatures.
-            if i >= listSpaces:#make sure the list remains at 2 mins. Min, Max and avg only need an update at 2min intervals
-                avgTemp = numpy.mean(payload)
-                payload.clear()
-                print("Cleared")
-                i = 0
+def outputer():
+    launch = time.time()
+    start = datetime.datetime.utcnow().isoformat()
+    payload = []  # List for all the data
+    # while time is less than 2 mins from start..
+    while time.time() < launch+intervalTime:
+        with open(infile, 'r') as measures:
+            for line in measures:
+                if time.time() > launch+intervalTime:
+                    break
+                else:
+                    payload.append((100/totbits)*float(line)-50)
+                    time.sleep(slpTime_reads)
+                    # print(payload)
+    return payload, start, datetime.datetime.utcnow().isoformat()
+
+# The final value for return is a direct funtion in stead of variable like the "start"-one so that it is not called pre-return, hence giving
+# a new value for the call
+
+# HTTP-posting of values
+
+
+def Sendres(pl, url):
+    request = requests.post(url, json=pl)
+    return request.status_code
+
+# returns the min, max and avg with rounding to 2 decimals
+
+
+def extraOutput(data):
+    max = np.round(float(np.max(data)), 2)
+    min = np.round(float(np.min(data)), 2)
+    avg = np.round(float(np.mean(data)), 2)
+    return max, min, avg
+
+
+# prepare data for shipment in Json form
+def formatforJson(max, min, avg, startTime, endTime):
+    data = {
+        "time": {
+            "start": str(startTime),
+            "end": str(endTime)
+        },
+        "min": min,
+        "max": max,
+        "avg": avg
+    }
+    return data
+
+# Backup for failed sends. Holds up to 10 at a time. Pops out the first one added when full
+
+
+def backup(rcv, bup):
+    if(len(bup) > 9):
+        bup.pop(-1)
+    bup.append(rcv)
+    return bup
+
+
+def main():
+    archive = []
+    sendfail = 0
+    while True:
+        data, start, end = outputer()
+        max, min, avg = extraOutput(data)
+        tosend = formatforJson(max, min, avg, start, end)
+        archive = backup(tosend, archive)
+
+        if sendfail >= 1:
+            retry = Sendres(archive[-1], targetUrl)
+            print("Trying again")
+            if retry != 200:
+                uhoh = Sendres(archive, backupUrl)
+                print("Retry failed, sending to backupserver")
             else:
-                maxlist = max(payload)
-                minlist = min(payload)
-                print(numpy.mean(payload), maxlist, minlist)
-                i+=1
+                print("retry succesful!")
+        post = Sendres(tosend, targetUrl)
+        if post != 200:
+            sendfail += 1
+            print("Post error, retry on next post")
+        else:
+            sendfail = 0
+            print("Post success!")
 
-getTemperature()
 
-#if r.status_code == 200:
-#    post = requests.post('https://httpbin.org/post', data=str(payload[i]))
-#    print(post.text)
-#    time.sleep(0.1)
-#elif r.status_code == 500:
-#    post = requests.post('https://httpbin.org/dump', data=str(payload[i]))
+main()
